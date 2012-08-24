62de732ba812379457f3e4e47542b8062344d175
eller86 <skypencil@gmail.com>
2012-08-24 23:00:03 +0800

    added a Rakefile to test

--- y2p.rb
+++ y2p.rb
@@ -13,34 +13,43 @@ if ! File.exist?(inFile)
   abort("input file %s does not exist" % inFile)
 end
 
-messageFileMap =
-  {"en" => File.open("Messages.properties", mode="w"),
-  "zh" => File.open("Messages_zh.properties", mode="w"),
-  "ja" => File.open("Messages_ja.properties", mode="w")}
+# see http://stackoverflow.com/questions/582686/should-i-define-a-main-method-in-my-ruby-scripts
+if __FILE__ == $0
+  y2p = Yaml2Properties.new()
+  y2p.run(inFile)
+end
 
-messageObj = YAML::load(File.open(inFile));
+class Yaml2Properties
+  def run(inFile)
+    messageFileMap =
+      {"en" => File.open("Messages.properties", mode="w"),
+      "zh" => File.open("Messages_zh.properties", mode="w"),
+      "ja" => File.open("Messages_ja.properties", mode="w")};
 
-messageObj.each_pair do |key, messageMap|
-  messageMap.each_pair do |lang, message|
-    s = ""
-    message = message || ""
+    messageObj = YAML::load(File.open(inFile));
 
-    messageFileMap[lang].write(key + "=")
+    messageObj.each_pair do |key, messageMap|
+      messageMap.each_pair do |lang, message|
+        s = ""
+        message = message || ""
 
-    if lang == "en"
-      s = message
-    else
-      if RUBY_VERSION >= "1.9.0"
-        message.codepoints {|c| s += "\\u" + ("%04x" % c)}
-      else
-        message.unpack("U*").each {|c| s += "\\u" + ("%04x" % c)}
+        messageFileMap[lang].write(key + "=")
+
+        if lang == "en"
+          s = message
+        else
+          if RUBY_VERSION >= "1.9.0"
+            message.codepoints {|c| s += "\\u" + ("%04x" % c)}
+          else
+            message.unpack("U*").each {|c| s += "\\u" + ("%04x" % c)}
+          end
+        end
+        messageFileMap[lang].write(s + "\n")
       end
     end
 
-    messageFileMap[lang].write(s + "\n")
+    messageFileMap.each_value do |f|
+      f.close
+    end
   end
-end
-
-messageFileMap.each_value do |f|
-  f.close
 end
---
+++ y2p_spec.rb
@@ -0,0 +1,29 @@
+﻿#!/usr/bin/env ruby
+require './y2p'
+
+describe Yaml2Properties, 'normal behavor' do
+  before do
+    @y2p = Yaml2Properties.new()
+  end
+  it 'create Messages_zh.properties' do
+    @y2p.run('test.yml')
+    open('Messages_zh.properties') {|file|
+      file.readlines[0].should == "my.test.message.warn=\\u8b66\\u544a\\u4fe1\\u606f\n"
+    }
+  end
+  it 'create Messages_ja.properties' do
+    @y2p.run('test.yml')
+    open('Messages_ja.properties') {|file|
+      file.readlines[0].should == "my.test.message.warn=\\u8b66\\u544a\\u30e1\\u30c3\\u30bb\\u30fc\\u30b8\n"
+    }
+  end
+  it 'create Messages.properties' do
+    @y2p.run('test.yml')
+    open('Messages.properties') {|file|
+      file.readlines[0].should == "my.test.message.warn=Warning message\n"
+    }
+  end
+  after do
+    File.delete('Messages_zh.properties', 'Messages_ja.properties', 'Messages.properties')
+  end
+end
